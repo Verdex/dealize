82423932f78a563d3cfc2bb312ec569928939b56
David L. Inman <davidlevere@gmail.com>
2024-03-02 00:34:23 -0500

    inc prog

--- src/jerboa.rs
+++ src/jerboa.rs
@@ -8,28 +8,20 @@ pub enum Capture<'a, T> {
 }
 
 #[derive(Clone)]
-pub enum Rule<T> {
+pub enum Match<T> {
     Free(Rc<dyn Fn(&T) -> bool>),
     Context(Rc<dyn for<'a> Fn(&T, &[Capture<'a, T>]) -> bool>),
-    Option(Box<Rule<T>>),
-    List(Box<Rule<T>>),
+    Option(Box<Match<T>>),
+    List(Box<Match<T>>),
 }
 
-pub struct Parser<T> {
-    rule : Rule<T>,
-    transform : fn,
+pub struct Rule<T, S> { // TODO should fields be public or should there be some sort of constructor?
+    matches: Vec<Match<T>>,
+    transform : Rc<dyn for<'a> Fn(&[Capture<'a, T>]) -> S>,
 }
 
-#[derive(Clone)]
-pub enum Blarg<'a, T> {
-    Blargy(&'a dyn Fn(&T) -> bool),
+pub fn parse<T, S>(input : &[T], rules: &[Rule<T, S>]) -> Result<Vec<S>, Box<dyn std::error::Error>> { // TODO error typet 
+    for rule in rules {
+
+    }
 }
-
-pub fn parse<T>(input : &[T], ) {
-
-}
-
-/*pub enum Parser<'a, I, O, E> {
-
-}
-*/
